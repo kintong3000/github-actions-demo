cf5b19792ba89abf05beb9267bff123ef4de9aec
KT <cheungkintong3000@gmail.com>
2023-02-23 00:43:51 +0800

    t

--- HW.sh
+++ HW.sh
@@ -2,5 +2,5 @@
 echo "FROM ubuntu
 CMD echo "hello world!" " >> Dockerfile #构建Dockerfile
 
-docker image build -t docker_hw #创建 image 文件
+docker image build -t docker_hw . #创建 image 文件
 docker container run docker_hw #生成容器
